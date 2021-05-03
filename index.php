6c97a30484750e1366cecf8a9f3d0905c5b8f768
KG <maclarenkung@gmail.com>
2021-05-03 19:58:27 +0700

    upate home---

--- index.php
+++ index.php
@@ -50,27 +50,90 @@
         </div>
         <div class="ourservice container">
             <div class="head-top text-center">
-                <p>Welcome to</p>
-                <h2>Kandarlin Clinic</h2>
+                <p>Our</p>
+                <h2>Service</h2>
 
             </div>
             <div class="tent">
-                <img src="assets/image/bg-welcome.jpg" alt="" class="img-welcome img-fluid">
-                <div class="text-rightt container">
-                    <div class="headtop">
-                        <h1>Kandarlin Clinic</h1>
+                <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+                 <div class="item">
+                    <img src="assets/image/ourservice.jpg" alt="" class="img-our img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="sec-banner  w-100 pt-4">
+            <div class="position-relative">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
                     </div>
-                    <div class="textbottom">
-                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti quod nam odit! Cumque ad tenetur voluptates ipsa commodi, voluptatibus quis sit, voluptate culpa obcaecati rerum rem repudiandae dignissimos sint.</span>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+        <div class="ourservice container">
+            <div class="head-top text-center">
+                <p>patient</p>
+                <h2>Testimonails</h2>
+            </div>
+            <div class="tent">
+               <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
                     </div>
-                    <div class="textbottom">
-                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti quod nam odit! Cumque ad tenetur voluptates ipsa commodi, voluptatibus quis sit, voluptate culpa obcaecati rerum rem repudiandae dignissimos sint.</span>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+            <div class="tent-b">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="assets/image/bgs.jpg"
+                                alt="" class="float-left w-100">
+                        </div>
                     </div>
-                    <div class="button">
-                        <a href="#" class="btn">
-                            READ MORE >
-                        </a>
-                    </div>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
                 </div>
             </div>
         </div>
@@ -167,6 +230,8 @@
                 </div>
             </div>
         </div>
+
+      
         <!-- <div class="row" id="body-row">
             <div id="sidebar-container" class="sidebar-expanded d-md-block d-none">
                 <ul class="list-group">
