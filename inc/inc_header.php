78b50a7bb8e2af89c279574876e454521a3e66f4
KG <maclarenkung@gmail.com>
2021-05-03 21:34:44 +0700

    merge

--- inc/inc_header.php
+++ inc/inc_header.php
@@ -1,41 +1,43 @@
 
-<div class="menu container" id="headers">
-    <nav class="navbar navbar-expand-lg navbar-light ">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">LOGO</a>
+<div class="menu " id="headers">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">LOGO</a>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      
-    </ul>
-    <ul class="form-inlines my-2 my-lg-0">
-     <li class="nav-item active">
-        <a class="nav-link" href="#">Home </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Service</a>
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Promotion </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Review</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Shop</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">News</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact Us</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                
+                </ul>
+                <ul class="form-inlines my-2 my-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#">Service</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Promotion </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Review</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#">Shop</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </div>
